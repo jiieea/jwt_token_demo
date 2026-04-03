f08c7921f482c079e3ce23778439c5246166f681
Jiéyra De Châtillon <seigneur.noir15@gmail.com>
2026-04-04 01:23:58 +0700

    feat : multer error handling

--- src/user/user.filter.ts
+++ src/user/user.filter.ts
@@ -5,18 +5,30 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { ZodError } from 'zod';
+import { Response } from 'express';
+import multer from 'multer';
 
 @Catch(ZodError, HttpException)
 export class UserFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
-    const response = host.switchToHttp().getResponse();
+    const response = host.switchToHttp().getResponse<Response>();
     if (exception instanceof HttpException) {
-      response.status(exception.getStatus()).json({
-        errors: exception.getResponse(),
+      const status = exception.getStatus();
+      const typeResponse = exception.getResponse();
+      const errMsg =
+        typeof typeResponse === 'object'
+          ? (typeResponse as any).message
+          : typeResponse;
+      response.status(status).json({
+        errors: errMsg,
       });
     } else if (exception instanceof ZodError) {
       response.status(400).json({
         errors: exception.issues,
+      });
+    } else if (exception instanceof multer.MulterError) {
+      response.status(413).json({
+        errors: exception.message,
       });
     } else {
       response.status(500).json({
